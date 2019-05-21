c44d2e08d519d11c33957bada0c738058fcd5b3b
Ustin <blacktree@bk.ru>
2019-05-22 00:06:47 +0500

    Добавлен цикл

--- ferst.js
+++ ferst.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?', '');
+let money = +prompt('Ваш бюджет на месяц?', '');
 let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -12,16 +12,32 @@ let appData = {
     savings: false,
 };
 
-for (let i = 0; i < 0; i++) {
+for (let i = 0; i < 2; i++) {
     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
         b = prompt('Во сколько обойдется?', '');
-        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && typeof(b) != null
-        && a != '' && b != '' && a.length < 50 ) {
-            console.log('Done');
-            appData.extenses[a] = b;
-        } else {
-            
-        }
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) === 'string' && typeof(b) != null 
+    && a != '' && b != '' && a.length < 50 ) {
+        console.log('Done');
+        appData.extenses[a] = b;
+    } else {
+    }
 };
 
-alert(appData.budget/30);
+appData.moneyPerDay = appData.budget/30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay <= 1000) {
+    console.log('Минимальный уровень достатка');
+    
+} else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 3000) {
+    console.log('Средний уровень достатка');
+    
+} else if (appData.moneyPerDay > 3000) {
+    console.log('Высокий уровень достатка');
+    
+} else {
+    console.log('Произошла ошибка');
+    
+}
